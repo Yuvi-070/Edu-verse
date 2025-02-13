e401e63ed1f544430727b5c78017717cbe563d3f
yuvi054 <taleyuvraj48@gmail.com>
2025-02-13 17:59:40 +0530

    After Sign-in added

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -9,7 +9,8 @@
 
 import Preloader from '@/components/common/Preloader'
 
-import HeaderAuth from '@/components/layout/headers/HeaderAuth'
+
+import HeaderFive from '@/components/layout/headers/HeaderFive'
 import AuthImageMove from '@/components/others/AuthImageMove'
 import { SignIn, SignUp } from '@clerk/nextjs'
 import React from 'react'
@@ -24,7 +25,7 @@ export default function page() {
     <div className="main-content  ">
       <Preloader/>
 
-        <HeaderAuth/>
+        <HeaderFive/>
         <div className="content-wrapper js-content-wrapper overflow-hidden">
             <section  className="form-page js-mouse-move-container">
                 <AuthImageMove/>
