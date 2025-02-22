dbefaf3c0003b9453cf8054537b537a434d07971
omkxrrr <omkarmedhe50@gmail.com>
2025-02-22 17:54:10 +0530

    Add courses

--- components/courseList/CourseListFive.jsx
+++ components/courseList/CourseListFive.jsx
@@ -187,7 +187,7 @@ export default function CourseListFive() {
 
                 <div>
                   <p className="page-header__text">
-                  Learn Python, Java, and C++ fundamentals, master data structures like arrays, linked lists, and trees, and explore UI/UX design for creating user-friendly digital experiences—all in one course!
+                    Learn from Top Youtubes.
                   </p>
                 </div>
               </div>
